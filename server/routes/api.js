7c93d15b63c993788ad390454096851c35a61a92
z0040cc <harish.chava@target.com>
2020-09-29 19:36:56 -0500

    updated pg db connection details

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,146 +1,117 @@
 const express = require('express');
 const router = express.Router();
-const Pool = require('pg').Pool;
-const queryFormat = require('pg-format');
+const Pool = require('pg').Pool
+const pg = require('pg')
+const pwd = 'hskpls@123'
+// const client = new pg.Client(`postgres://hashkey_db:${pwd}@10.59.15.6:5432/hashkey_db?ssl=true&sslmode=require`)
+
+// client.connect(function (err) {
+//     if (err) {
+//         return console.error('could not connect to DB', err);
+//     }
+//     client.query('SELECT * FROM ttd_ingestion.ttd_file_metadata ORDER BY id ASC', (error, results) => {
+//         if (error) {
+//             rej(error);
+//         }
+//
+//         client.end();
+//     });
+// });
+
 const pool = new Pool({
-  user: 'chava',
-  host: 'localhost',
-  database: 'files',
-  password: 'bavagadu',
-  port: 5432,
+    user: 'hashkey_db',
+    host: '10.59.15.6',
+    database: 'hashkey_db',
+    password: 'hskpls@123',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-const delIds = [1,2,3,4,5,6,7,8];
+
 // const router = express.Router();
-function dataFetch () {
+function dataFetch() {
     return new Promise((res, rej) => {
-        pool.query('SELECT * FROM filesinfo ORDER BY id ASC', (error, results) => {
+        pool.query('SELECT * FROM ttd_ingestion.ttd_file_metadata WHERE status=\'N\' limit 100', (error, results) => {
             if (error) {
+                console.error('error connecting', error.stack)
                 rej(error);
             }
             res(results.rows);
         });
     });
-} 
-function getLandingData(req, res, next) {
-   return dataFetch().then(rows => {
-       res.json(rows);
-   }).catch(err => {
-       res.status(500).json(err);
-   });
 }
+
+function getLandingData(req, res, next) {
+    return dataFetch().then(rows => {
+        res.json(rows);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+}
+
 function addFile(req, res, next) {
     return new Promise((res, rej) => {
-        const { file_name, status  } = req.body;
-        pool.query('INSERT INTO filesinfo (file_name, status) VALUES ($1, $2)', [file_name, status], (error, results) => {
+        const {file_name, status} = req.body;
+        pool.query('INSERT INTO ttd_ingestion.ttd_file_metadata (file_name, status) VALUES ($1, $2)', [file_name, status], (error, results) => {
             if (error) {
-              rej(error);
+                rej(error);
             }
             res(results);
         });
     })
-    .then(dataFetch)
-    .then(rows => {
-        res.locals.newData = rows;
-        next();
-    }).catch(err => {
-        res.status(500).json(err);
-    });
-}
-function addFiles(req, res, next) {
-    return new Promise((res, rej) => {
-        const newRecords = req.body.addRecs && req.body.addRecs.length ? req.body.addRecs : require('../../addRecords.json').addRecords;
-        const values = [];
-        newRecords.forEach(rec => {
-            const date = new Date();
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-            const day = date.getDay();
-            const hours = date.getHours();
-            const minutes = date.getMinuites();
-            const seconds = date.getSeconds();
-            const milliseconds = date.getMilliseconds();
-            const lastModified = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
-            const itrVals = [rec.file_name, rec.file_size, lastModified, rec.status, rec.file_type];
-            values.push(itrVals);
+        .then(dataFetch)
+        .then(rows => {
+            res.locals.newData = rows;
+            next();
+        }).catch(err => {
+            res.status(500).json(err);
         });
-        const addFilesQuery = queryFormat('INSERT INTO filesinfo (file_name, file_size, last_modified, status, file_type) VALUES %L', values);
-        pool.query(addFilesQuery, (error, results) => {
-            if (error) {
-              rej(error);
-            }
-            res(results);
-        });
-    })
-    .then(dataFetch)
-    .then(rows => {
-        res.locals.newData = rows;
-        next();
-    }).catch(err => {
-        res.status(500).json(err);
-    });
 }
+
 function changeFile(req, res, next) {
     return new Promise((res, rej) => {
         const id = parseInt(req.params.id)
-        const { file_name, status  } = req.body;
-        pool.query('UPDATE filesinfo SET file_name = $1, status = $2 WHERE id = $3', [file_name, status, id], (error, results) => {
+        const {file_name, status} = req.body;
+        pool.query('UPDATE ttd_ingestion.ttd_file_metadata SET file_name = $1, status = $2 WHERE id = $3', [file_name, status, id], (error, results) => {
             if (error) {
-              rej(error);
-            } 
+                rej(error);
+            }
             res(results);
         });
     })
-    .then(dataFetch)
-    .then(rows => {
-        res.locals.newData = rows;
-        next();
-    }).catch(err => {
-        res.status(500).json(err);
-    });
+        .then(dataFetch)
+        .then(rows => {
+            res.locals.newData = rows;
+            next();
+        }).catch(err => {
+            res.status(500).json(err);
+        });
 }
+
 function deleteFile(req, res, next) {
     return new Promise((res, rej) => {
         const id = parseInt(req.params.id)
-        pool.query('DELETE FROM filesinfo WHERE id = $1', [id], (error, results) => {
+        pool.query('DELETE FROM ttd_ingestion.ttd_file_metadata WHERE id = $1', [id], (error, results) => {
             if (error) {
-              rej(error);
+                rej(error);
             }
             res(results);
         });
     })
-    .then(dataFetch)
-    .then(rows => {
-        res.locals.newData = rows;
-        next();
-    }).catch(err => {
-        res.status(500).json(err);
-    });
-}
-function deleteFiles(req, res, next) {
-    return new Promise((res, rej) => {
-        const ids = (req.body.delIds && req.body.delIds.length) ? req.body.delIds : delIds;
-        const delQuery = queryFormat('DELETE FROM filesinfo WHERE id IN %L', [ids]);
-        pool.query(delQuery, (error, results) => {
-            if (error) {
-              rej(error);
-            }
-            res(results);
+        .then(dataFetch)
+        .then(rows => {
+            res.locals.newData = rows;
+            next();
+        }).catch(err => {
+            res.status(500).json(err);
         });
-    })
-    .then(dataFetch)
-    .then(rows => {
-        res.locals.newData = rows;
-        next();
-    }).catch(err => {
-        res.status(500).json(err);
-    });
 }
+
 /* GET home page. */
 router.get('/', getLandingData);
 router.post('/addFile', addFile);
-router.post('/addFiles', addFiles);
 router.put('/changeFile/:id', changeFile);
 router.delete('/deleteFile/:id', deleteFile);
-router.post('/deleteFiles', deleteFiles);
 
 module.exports = router;
